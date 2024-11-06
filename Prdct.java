1e22ce3b5fda7f374620ecdc6fe2bc4187daa0cc
Chennakesavulu <134907965+L143EGEND@users.noreply.github.com>
2024-11-06 21:39:48 +0530

    Add files via upload

---
+++ Prdct.java
@@ -0,0 +1,13 @@
+class Prdct {
+    public static void main(String args[]) {
+        String Prdct_name = "COLGATE";
+        long Prdct_id = 5463734352L;
+        float Prdct_price = 99.99F; // Corrected variable name
+        String Prdct_category = "TOOTH PASTE";
+        
+        System.out.println("Given Product Name: " + Prdct_name);
+        System.out.println("Given Product ID: " + Prdct_id);
+        System.out.println("Given Product PRICE: " + Prdct_price);
+        System.out.println("Given Product CATEGORY: " + Prdct_category);
+    }
+}
